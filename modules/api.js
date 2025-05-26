5c7358c7530427602fad1ca59a1c9ed65d466eb8
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-27 00:27:07 +0200

    Set up the API with the role of fetching tasks

---
+++ modules/api.js
@@ -0,0 +1,13 @@
+export async function fetchTasks() {
+  try {
+    const response = await fetch("https://jsl-kanban-api.vercel.app");
+    if (!response.ok) {
+      throw new Error("Failed to fetch tasks");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    return [];
+  }
+}
