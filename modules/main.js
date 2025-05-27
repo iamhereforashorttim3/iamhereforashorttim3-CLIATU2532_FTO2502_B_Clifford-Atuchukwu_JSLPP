f3c8393f01c7efb797f0d2e8374a0e31d129b0ad
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-27 18:51:16 +0200

    Updated the main.js to properly fetch tasks from the API and store them in the localStorage with error and loading states

--- modules/main.js
+++ modules/main.js
@@ -1,1 +1,33 @@
-import { fetchTasks } from "./api";
+/**
+ * Function imports tasks from the API
+ */
+import { fetchTasks } from "./api/api";
+
+const loading = document.createElement("div");
+loading.id = "loading";
+loading.textContent = "Tasks loading...";
+document.body.appendChild(loading);
+
+async function startApp() {
+  try {
+    const tasks = await fetchTasks();
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    console.log("Tasks loaded:", tasks);
+    //This removes the loading message if the tasks are loaded
+    const loadingElement = document.getElementById("loading");
+    if (loadingElement) loadingElement.remove();
+  } catch (error) {
+    /**
+     * In case of error, the loading is removed and an error message will show
+     */
+    console.log("Failed to load tasks:", error);
+    const loadingElement = document.getElementById("loading");
+    if (loadingElement) loadingElement.remove();
+    document.body.innerHTML =
+      '<p style="color:red;">Failed to load tasks. Please try again later.</p>';
+  }
+}
+
+startApp();
