d0aad0d5e4b3244926f47b6abfefb6296cdec270
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-29 07:47:27 +0200

    Added task ID and description handling to ensure modal opens with full task data

--- modules/ui/modal.js
+++ modules/ui/modal.js
@@ -1,15 +1,19 @@
-export function handleTaskClick(task) {
+export function handleTaskClick(e) {
   const titleElement = document.getElementById("task-title");
   const descriptionElement = document.getElementById("task-description");
   const statusElement = document.getElementById("task-status");
+  const modal = document.getElementById("task-modal");
 
   const title = e.currentTarget.dataset.taskTitle;
-  const description = e.currentTarget.dataset.TaskDescription;
+  const description = e.currentTarget.dataset.taskDescription;
   const status = e.currentTarget.dataset.taskStatus;
+  const id = e.currentTarget.dataset.taskId;
 
   titleElement.value = title;
   descriptionElement.value = description;
   statusElement.value = status;
+
+  modal.dataset.taskId = id;
 
   document.getElementById("task-modal").showModal();
 }
