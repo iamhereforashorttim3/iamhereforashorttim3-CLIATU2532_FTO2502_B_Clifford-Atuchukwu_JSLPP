38028c80f42c049415c1c46d8a93fa39ef7d3648
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-29 22:00:51 +0200

    added function and click listeners for theme

---
+++ modules/ui/theme.js
@@ -0,0 +1,24 @@
+export function themeToggleSwitch() {
+  let darkmode = localStorage.getItem("darkmode");
+  const themeSwitch = document.getElementById("theme-toggle");
+
+  const enableDarkmode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+    if (logo) logo.src = "./assets/logo-dark.svg";
+  };
+
+  const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+    if (logo) logo.src = "./assets/logo-light (1).svg";
+  };
+
+  if (darkmode === "active") enableDarkmode();
+  else disableDarkMode();
+
+  themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkmode() : disableDarkMode();
+  });
+}
