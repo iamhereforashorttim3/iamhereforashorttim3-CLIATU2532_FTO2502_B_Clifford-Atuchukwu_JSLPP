a51dc1a122b486e68b95446e31c222c80f567ca6
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-30 09:39:04 +0200

    Added If lines that switch icon when the theme is toggled

--- modules/ui/theme.js
+++ modules/ui/theme.js
@@ -4,16 +4,23 @@ export function themeToggleSwitch() {
   let darkmode = localStorage.getItem("darkmode");
   const themeSwitch = document.getElementById("theme-toggle");
 
+  const logo = document.getElementById("logo");
+  const mobileSvgIcon = document.getElementById("mobile-svg-icon");
+
   const enableDarkmode = () => {
     document.body.classList.add("darkmode");
     localStorage.setItem("darkmode", "active");
+
     if (logo) logo.src = "./assets/logo-dark.svg";
+    if (mobileSvgIcon) mobileSvgIcon.src = "./assets/logo-dark.svg";
   };
 
   const disableDarkMode = () => {
     document.body.classList.remove("darkmode");
     localStorage.setItem("darkmode", null);
+
     if (logo) logo.src = "./assets/logo-light (1).svg";
+    if (mobileSvgIcon) mobileSvgIcon.src = "./assets/favicon (3).svg";
   };
 
   if (darkmode === "active") {
